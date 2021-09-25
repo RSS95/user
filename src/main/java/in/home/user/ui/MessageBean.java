9043c7d6785626724574f6e33bc1ef2039e47e5f
Ritesh Sadhu Salian <rsalian95@gmail.com>
2021-09-25 22:11:37 +0530

    added serializable to message bean, to remove startup serialization error

--- src/main/java/in/home/user/ui/MessageBean.java
+++ src/main/java/in/home/user/ui/MessageBean.java
@@ -1,10 +1,13 @@
 package in.home.user.ui;
 
+import java.io.Serializable;
 import java.time.LocalTime;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MessageBean {
+public class MessageBean implements Serializable {
+
+  public static final long serialVersionUID = 1L;
 
   public String getMessage() {
     return "Button was clicked at " + LocalTime.now();
